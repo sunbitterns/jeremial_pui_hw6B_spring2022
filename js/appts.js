cc376d01058c5b3bc603dcedb193d9bd60b93bff
sunbitterns <lojeremia@gmail.com>
2022-04-04 12:09:34 -0400

    cancel function

--- js/appts.js
+++ js/appts.js
@@ -1,3 +1,5 @@
+let appts;
+
 /* Sort array based on date & time
    Citation: https://stackoverflow.com/questions/6913512/how-to-sort-an-array-of-objects-by-multiple-fields
 */
@@ -11,7 +13,7 @@ function sortAppts(appts) {
    Citation: https://stackoverflow.com/questions/29335369/display-array-of-objects-in-a-dynamic-table-javascript
 */
 function generateApptTable() {
-    let appts = JSON.parse(localStorage.getItem("appts"));    
+    appts = JSON.parse(localStorage.getItem("appts"));    
     sortAppts(appts);
     localStorage.setItem("appts", JSON.stringify(appts));
 
@@ -28,10 +30,10 @@ function generateApptTable() {
     }
     html+="</table>";
 
-
     document.getElementById("appts").innerHTML = html;
 }
 
+/* Get index from button row */
 function getIndex(element) {
     let index = element.parentNode.parentNode.rowIndex;
     localStorage.setItem("apptIndex", JSON.stringify(index));
@@ -39,7 +41,7 @@ function getIndex(element) {
    
 /* Load appointments */
 function loadAppts() {
-    let appts = JSON.parse(localStorage.getItem("appts"));
+    appts = JSON.parse(localStorage.getItem("appts"));
     if (appts == null || appts == []) {
         document.getElementById("appts").innerHTML = 
         "No appointment history."
@@ -52,23 +54,22 @@ function loadAppts() {
 function cancelAppt(i) {
     appts.splice(i, 1);
     localStorage.setItem("appts", JSON.stringify(appts));
+    //location.reload();
 }
 
 /* Load appointment detail */
 function loadApptDetail() {
-    let appts = JSON.parse(localStorage.getItem("appts"));
+    appts = JSON.parse(localStorage.getItem("appts"));
     let apptIndex = localStorage.getItem("apptIndex");
 
+    /* Retrieve selected appt
+       Not sure why my indices are by doubles...*/
     let selAppt = "";
     if (apptIndex == 0) {
         selAppt = appts[0];
     } else {
         selAppt = appts[apptIndex/2];
     }
-
-    console.log(appts);
-    console.log(apptIndex);
-    console.log(selAppt);
 
     // Display appointment details 
     document.getElementById("dateProp").innerHTML = 
@@ -79,4 +80,8 @@ function loadApptDetail() {
         "<strong>Visit Type:</strong> " + selAppt.type;
     document.getElementById("locationProp").innerHTML = 
         "<strong>Location:</strong> " + selAppt.location;
+
+    // Cancel button functionality
+    let cancelBtn = document.getElementById("cancel");
+    cancelBtn.setAttribute("onclick", `cancelAppt("${apptIndex}")`);
 }
